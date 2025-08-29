62616f76f7a10f19c25fad98d47de816e5b6173e
MelinaLa <shiniychu@gmail.com>
2025-08-29 11:14:37 +0200

    kgc

--- test_password_checker.py
+++ test_password_checker.py
@@ -20,3 +20,26 @@ def test_no_lowercase():
 def test_no_special_char():
 	assert is_valid_password('Abcdef12') == False
 
+# ////
+
+def test_long_password():
+	assert is_valid_password('Abcdef1!' * 10) == True
+
+def test_password_with_spaces():
+	assert is_valid_password('Abc def1!') == True
+
+def test_only_special_chars():
+	assert is_valid_password('!@#$%^&*') == False
+
+def test_only_digits():
+	assert is_valid_password('12345678') == False
+
+def test_only_letters():
+	assert is_valid_password('Abcdefgh') == False
+
+def test_unicode_characters():
+	assert is_valid_password('Äbcdef1!') == True
+
+def test_empty_string():
+	assert is_valid_password('') == False
+
