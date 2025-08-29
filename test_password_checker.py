ef4b4792613a076f0060f455f7fca497b482a724
MelinaLa <shiniychu@gmail.com>
2025-08-29 10:46:23 +0200

    ftgbf

--- test_password_checker.py
+++ test_password_checker.py
@@ -1,1 +1,22 @@
+# test_password_checker.py
+import pytest
+from password_checker import is_valid_password
+
+def test_valid_password():
+	assert is_valid_password('Abcdef1!') == True
+
+def test_too_short():
+	assert is_valid_password('Ab1!') == False
+
+def test_no_digit():
+	assert is_valid_password('Abcdefgh!') == False
+
+def test_no_uppercase():
+	assert is_valid_password('abcdef1!') == False
+
+def test_no_lowercase():
+	assert is_valid_password('ABCDEF1!') == False
+
+def test_no_special_char():
+	assert is_valid_password('Abcdef12') == False
 
